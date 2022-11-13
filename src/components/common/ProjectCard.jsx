4c1ff18db5b76352a608899b0dded615363566b0
algertc <algertc@gmail.com>
2022-11-13 14:55:57 -0800

    trans timing

--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -8,7 +8,7 @@ export default function Card({ link, title, description, image }) {
       <div class="group max-w-sm rounded-lg overflow-hidden shadow-lg hover:bg-gradient-to-r from-rose-400 via-m2-200 to-indigo-500 animate-gradient-x p-2 cursor-pointer scale-101 project-card">
         <div class="group w-full rounded-lg max-w-sm overflow-hidden">
         <img
-          class="w-full rounded-lg object-cover group-hover:scale-105 "
+          class="w-full rounded-lg object-cover transform transition duration-400 group-hover:scale-105 "
           src={image.src}
           alt="Sunset in the mountains"
           style={{ height: 110 }}
