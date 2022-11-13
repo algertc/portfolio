607a291846d6d87303e9887335adc0519aee0d8d
algertc <algertc@gmail.com>
2022-11-13 14:50:19 -0800

    add group hover effect for image zoom on projects

--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -6,9 +6,9 @@ export default function Card({ link, title, description, image }) {
       className="transform transition duration-500 hover:scale-105"
     >
       <div class="group max-w-sm rounded-lg overflow-hidden shadow-lg hover:bg-gradient-to-r from-rose-400 via-m2-200 to-indigo-500 animate-gradient-x p-2 cursor-pointer scale-101 project-card">
-        <div class="w-full rounded-lg">
+        <div class="group w-full rounded-lg max-w-sm overflow-hidden">
         <img
-          class="w-full rounded-lg object-cover"
+          class="w-full rounded-lg object-cover group-hover:scale-105 "
           src={image.src}
           alt="Sunset in the mountains"
           style={{ height: 110 }}
