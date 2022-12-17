55169d0d16b44b34375d1804b7af076410d65701
algertc <algertc@gmail.com>
2022-12-17 02:41:46 -0800

    fix analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
+            <script defer data-domain="charliealgert.com" src="https://charliealgert.com/js/script.js"></script>
             <body className="bg-black text-neutral-300">
                 <Main />
                 <NextScript />
