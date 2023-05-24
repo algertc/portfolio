4cff9e8663271e1df033beba9533289904f3da75
algertc <charliealgert@Charlies-Mac-Studio.local>
2023-05-23 19:57:47 -0700

    xxl scaling

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 export default function Hero() {
     return (
-        <section className="py-5 lg:py-6 md:py-15 xl:py-0 lg:pr-[3.2rem] xl:pb-24 xl:pr-[2rem] xxl:pr-[14rem]">
-            <div className="container grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 items-center gap-4 md:gap-32 lg:gap-24 xl:gap-0 xxl:gap-[13rem]">
+        <section className="py-5 lg:py-6 md:py-15 xl:py-0 lg:pr-[3.2rem] xl:pb-24 xl:pr-[2rem] xxl:pr-[0rem]">
+            <div className="container grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 items-center gap-4 md:gap-32 lg:gap-24 xl:gap-0 xxl:gap-[3rem]">
                 {/* <div> */}
                 <img
                     src={introduction.avatar.src}
@@ -13,7 +13,7 @@ export default function Hero() {
                     width={1060}
                     height={560}
                     // quality={100}
-                    className="sm:max-w-[320px] md:max-w-[450px] lg:max-w-[560px] xl:max-w-[62rem] xxl:max-w-[80rem] xxl:pl-24 mx-auto md:mx-0 lg:mx-auto xl:mx-10"
+                    className="sm:max-w-[320px] md:max-w-[450px] lg:max-w-[560px] xl:max-w-[62rem] xxl:max-w-[70rem] xxl:pb-32 xxl:ml-[24rem] mx-auto md:mx-0 lg:mx-auto xl:mx-10"
                 />
                 {/* </div> */}
                 <div>
@@ -27,7 +27,7 @@ export default function Hero() {
                         />
                     </h1>
 
-                    <p className="max-w-none md:max-w-[350px] mx-5 lg:max-w-[560px] xl:max-w-[48rem] xxl:max-w-[51rem] md:ml-0.5 xl:ml-16 xxl:ml-11 lg:ml-2 md:mr-8 lg:mr-5 text-center md:text-left lg:text-left text-xl lg:text-3xl xl:text-[3.5rem] xxl:text-[4.2rem] font-mono font-bold lg:font-extrabold xl:leading-[4.1rem] xxl:leading-[4.4rem] leading-30">
+                    <p className="max-w-none md:max-w-[350px] mx-5 lg:max-w-[560px] xl:max-w-[48rem] xxl:max-w-[51rem] md:ml-0.5 xl:ml-16 xxl:ml-11 lg:ml-2 md:mr-8 lg:mr-5 text-center md:text-left lg:text-left text-xl lg:text-3xl xl:text-[3.5rem] xxl:text-[3rem] font-mono font-bold lg:font-extrabold xl:leading-[4.1rem] xxl:leading-[4.4rem] xxl:pb-[12rem] leading-30">
                         {introduction.description}
                     </p>
                 </div>
